4eabbf0aca9b0114cce8742f17a89878019c1f79
sunthi <nonthekop96@gmail.com>
2019-02-10 04:01:37 +0700

    แก้ไข app module - close #76

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,25 @@
-import { RequestformController } from './controller/requestform-controller';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MatInputModule,MatCardModule,MatButtonModule,MatCheckboxModule,MatDatepickerModule,MatNativeDateModule,MatListModule,MatToolbarModule,MatGridListModule,MatProgressSpinnerModule,MatTableModule,MatSelectModule,MatOptionModule} from '@angular/material/';
+import { 
+        MatInputModule,
+        MatCardModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatDatepickerModule,
+        MatListModule,
+        MatToolbarModule,
+        MatGridListModule,
+        MatProgressSpinnerModule,
+        MatTableModule,
+        MatSelectModule,
+        MatOptionModule,
+        MatFormFieldModule,
+        MatRadioModule,
+        MatNativeDateModule
+} from '@angular/material/';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
@@ -17,6 +32,14 @@ import { MatDialogModule} from '@angular/material/dialog';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { OndutyComponent } from './onduty/onduty.component';
 import { RequestComponent } from './request/request.component';
+import { LogingComponent } from './components/loging/loging.component';
+import { LogingService } from './shared/loging/loging.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { HomemainComponent } from './components/homemain/homemain.component';
+import { WorkScheduleRegComponent } from './components/workSchedule-reg/workSchedule-reg.component';
+import { WorkScheduleRegService } from './shared/workSchedule-reg/workSchedule-reg.service';
+
 
 @NgModule({
   declarations: [
@@ -25,10 +48,17 @@ import { RequestComponent } from './request/request.component';
     LoginComponent,
     MainpageComponent,
     OndutyComponent,
-    RequestComponent
+    RequestComponent,
+    LogingComponent,
+    NavbarComponent,
+    HomeComponent,
+    HomemainComponent,
+    WorkScheduleRegComponent
   ],
   imports: [
+    HttpModule,
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MatButtonModule,
@@ -39,16 +69,19 @@ import { RequestComponent } from './request/request.component';
     MatToolbarModule,
     FormsModule,
     RouterModule,
-    AppRoutingModule,
     MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule,
     MatTableModule,
     MatCheckboxModule,
     MatProgressSpinnerModule,
-    MatOptionModule
+    MatOptionModule,
+    MatRadioModule
   ],
-  providers: [],
+  providers: [ 
+      LogingService,
+      WorkScheduleRegService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
