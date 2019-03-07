2d6647a6c3b952f9b3959047e3464d0f3a6b0135
kyosita <the.metal.barbies@gmail.com>
2019-03-07 06:59:24 +0000

    close #117 ทำ แก้ไขTestCase

--- backend/src/test/java/com/okta/developer/demo/PaymentinfoTest.java
+++ backend/src/test/java/com/okta/developer/demo/PaymentinfoTest.java
@@ -1,4 +1,4 @@
-﻿package com.sut.se.cpe.server.test;
+package com.sut.se.cpe.server;
 import lombok.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,6 +31,8 @@ public class PaymentinfoTest {
     @Autowired
     private PaymentinfoRepository paymentinfoRepository;
 
+
+
     private Validator validator;
 
     @Before
@@ -43,10 +45,10 @@ public class PaymentinfoTest {
     public void testDataSuccess(){
         Paymentinfo paymentinfo = new Paymentinfo();
         paymentinfo.setPayPrice("55555");
-        paymentinfo.setNote("pass");
-        paymentinfo.setMediList("ยาแก้แวด");;
+        paymentinfo.setNote("okay");
+        paymentinfo.setMediList("พาราเซตามอล");
         paymentinfo.setTreatList("รักษาอาการปวด");
-        paymentinfo.setSpecialNote("ส่วนลดพิเศษค่ารักษาพยาบาล");
+        paymentinfo.setSpecialNote("non");
 
         try {
             entityManager.persist(paymentinfo);
@@ -64,10 +66,10 @@ public class PaymentinfoTest {
     public void testsetsetPayPriceNotBeNull(){
         Paymentinfo paymentinfo = new Paymentinfo();
         paymentinfo.setPayPrice(null);
-        paymentinfo.setNote("pass");
-        paymentinfo.setMediList("ยาแก้แวด");;
+        paymentinfo.setNote("okay");
+        paymentinfo.setMediList("พาราเซตามอล");
         paymentinfo.setTreatList("รักษาอาการปวด");
-        paymentinfo.setSpecialNote("ส่วนลดพิเศษค่ารักษาพยาบาล");
+        paymentinfo.setSpecialNote("non");
 
         try {
             entityManager.persist(paymentinfo);
@@ -86,10 +88,10 @@ public class PaymentinfoTest {
     public void testsetsetPayPriceNotBeSize(){
         Paymentinfo paymentinfo = new Paymentinfo();
         paymentinfo.setPayPrice("4444444444444444444444444444444444444444444");
-        paymentinfo.setNote("pass");
-        paymentinfo.setMediList("ยาแก้แวด");;
+        paymentinfo.setNote("okay");
+        paymentinfo.setMediList("พาราเซตามอล");
         paymentinfo.setTreatList("รักษาอาการปวด");
-        paymentinfo.setSpecialNote("ส่วนลดพิเศษค่ารักษาพยาบาล");
+        paymentinfo.setSpecialNote("non");
 
         try {
             entityManager.persist(paymentinfo);
@@ -105,13 +107,13 @@ public class PaymentinfoTest {
         }
     }
     @Test
-    public void testsetsetPayPriceNotPattern(){
+    public void testsetPayPriceNotPattern(){
         Paymentinfo paymentinfo = new Paymentinfo();
         paymentinfo.setPayPrice("hhhhh");
-        paymentinfo.setNote("pass");
-        paymentinfo.setMediList("ยาแก้แวด");;
+        paymentinfo.setNote("okay");
+        paymentinfo.setMediList("พาราเซตามอล");
         paymentinfo.setTreatList("รักษาอาการปวด");
-        paymentinfo.setSpecialNote("ส่วนลดพิเศษค่ารักษาพยาบาล");
+        paymentinfo.setSpecialNote("non");
 
         try {
             entityManager.persist(paymentinfo);
@@ -126,5 +128,101 @@ public class PaymentinfoTest {
             assertEquals(violations.size(), 5);
         }
     }
+
+    @Test
+    public void testsetNoteNotNull(){
+        Paymentinfo paymentinfo = new Paymentinfo();
+        paymentinfo.setPayPrice("500");
+        paymentinfo.setNote(null);
+        paymentinfo.setMediList("พาราเซตามอล");
+        paymentinfo.setTreatList("รักษาอาการปวด");
+        paymentinfo.setSpecialNote("non");
+
+        try {
+            entityManager.persist(paymentinfo);
+            entityManager.flush();
+            //fail("Should not pass to this line");
+        }  catch(javax.validation.ConstraintViolationException e) {
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            System.out.println("\n\n======================>testsetsetPayPriceNotPattern<========================");
+            System.out.println(violations);
+            System.out.println("\n");
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 5);
+        }
+    }
+
+    @Test
+    public void testsetMediNotNull(){
+        Paymentinfo paymentinfo = new Paymentinfo();
+        paymentinfo.setPayPrice("500");
+        paymentinfo.setNote("none");
+        paymentinfo.setMediList(null);
+        paymentinfo.setTreatList("รักษาอาการปวด");
+        paymentinfo.setSpecialNote("non");
+
+        try {
+            entityManager.persist(paymentinfo);
+            entityManager.flush();
+            //fail("Should not pass to this line");
+        }  catch(javax.validation.ConstraintViolationException e) {
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            System.out.println("\n\n======================>testsetsetPayPriceNotPattern<========================");
+            System.out.println(violations);
+            System.out.println("\n");
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 5);
+        }
+    }
+
+    @Test
+    public void testsetTreatNotNull(){
+        Paymentinfo paymentinfo = new Paymentinfo();
+        paymentinfo.setPayPrice("500");
+        paymentinfo.setNote("non");
+        paymentinfo.setMediList("พาราเซตามอล");
+        paymentinfo.setTreatList(null);
+        paymentinfo.setSpecialNote("non");
+
+        try {
+            entityManager.persist(paymentinfo);
+            entityManager.flush();
+            //fail("Should not pass to this line");
+        }  catch(javax.validation.ConstraintViolationException e) {
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            System.out.println("\n\n======================>testsetsetPayPriceNotPattern<========================");
+            System.out.println(violations);
+            System.out.println("\n");
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 5);
+        }
+    }
+
+    @Test
+    public void testsetSpecialNotNull(){
+        Paymentinfo paymentinfo = new Paymentinfo();
+        paymentinfo.setPayPrice("500");
+        paymentinfo.setNote("non");
+        paymentinfo.setMediList("พาราเซตามอล");
+        paymentinfo.setTreatList("รักษาอาการปวด");
+        paymentinfo.setSpecialNote(null);
+
+        try {
+            entityManager.persist(paymentinfo);
+            entityManager.flush();
+            //fail("Should not pass to this line");
+        }  catch(javax.validation.ConstraintViolationException e) {
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            System.out.println("\n\n======================>testsetsetPayPriceNotPattern<========================");
+            System.out.println(violations);
+            System.out.println("\n");
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 5);
+        }
+    }
+
+
+
+
 
 }
